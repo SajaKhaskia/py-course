c0384e730c0756a81a5896d331c0cbf3703358cf
Saja <Saja.Khaskia93@gmail.com>
2023-04-20 19:13:15 +0300

    add readme

--- 01-basic/wordle/game.py
+++ 01-basic/wordle/game.py
@@ -1,4 +1,8 @@
+from words_manager import WordsManager
 import words as data
 
 print(f'number of words: {len(data.words_5_letters)}')
 print(f'first word: {data.words_5_letters[0]}')
+
+wm = WordsManager()
+print(f'random word: {wm.select_random()}')
---
+++ 01-basic/wordle/words_manager.py
@@ -0,0 +1,10 @@
+import words as data
+import random
+
+
+class WordsManager:
+    def __init__(self):
+        self.words = data.words_5_letters
+
+    def select_random(self):
+        return random.choice(self.words)

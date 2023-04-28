06c590154338500b7413bb45029a04ca980f43c8
Saja <Saja.Khaskia93@gmail.com>
2023-04-28 19:41:12 +0300

    start

--- 01-basic/wordle/game.py
+++ 01-basic/wordle/game.py
@@ -1,21 +1,20 @@
 from words_manager import WordsManager
-import words as data
+
 
 # print(f'Number of words: {len(data.words_5_letters)}')
 # print(f'First word: {data.words_5_letters[0]}')
 
 wm = WordsManager()
-secret = wm.select_random()
-print("*********")
-print(f'Random word: {secret}')
-print("*********")
+wm.print_secret()
+wm.print_instructions()
 
-print("Guess the Wordle in 6 tries.")
-print("Each guess must be a valid 5-letter word.")
+print("Let's start")
 for i in range(6):
-    word = input("Your guess: ")
-    if word == secret:
-        print("You win")
+    guess = input("Your guess: ").lower()
+    if guess == wm.secret:
+        print("You win!")
         break
     else:
-        print('Try again!')
+        wm.check_guess(guess)
+        print("\n")
+
--- 01-basic/wordle/words_manager.py
+++ 01-basic/wordle/words_manager.py
@@ -5,9 +5,28 @@ import random
 class WordsManager:
     def __init__(self):
         self.words = data.words_5_letters
+        self.secret = self.select_random()
 
     def select_random(self):
         return random.choice(self.words)
 
     def check_guess(self, guess):
-        pass
+        # print(f'Number of words: {len(guess)}')
+        for i in range(5):
+            for letter in guess:
+                if letter != self.secret[i]:
+                    letter = '_'
+                print(letter, end="")
+                i += 1
+            break
+
+    def print_secret(self):
+        print("*********")
+        print(f'Random word: {self.secret}')
+        print("*********")
+
+    def print_instructions(self):
+        print("How to play:")
+        print("Guess the Wordle in 6 tries.")
+        print("Each guess must be a valid 5-letter word.")
+        # print("The color of the tiles will change to show how close your guess was to the word.")
